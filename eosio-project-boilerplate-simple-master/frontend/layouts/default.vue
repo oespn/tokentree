8d6fab656c3a8f5d285321a4d55c0ed225c24744
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-08-04 15:55:33 +1000

    add gmap

---
+++ eosio-project-boilerplate-simple-master/frontend/layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-content>
+      <nuxt />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Toolbar from '~/components/Toolbar'
+export default {
+  components: {
+    Toolbar
+  }
+}
+</script>
+
+<style lang="stylus">
+  @import '~vuetify/src/stylus/main'
+</style>
---
+++ eosio-project-boilerplate-simple-master/frontend/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <GmapMap
+    :center="{lat:10, lng:10}"
+    :zoom="7"
+    map-type-id="terrain">
+  </GmapMap>
+</template>
+
+<style>
+.vue-map-container {
+  height: 100vh;
+  height: 100%;
+  width: 100vw;
+  width: 100%;
+}
+</style>
