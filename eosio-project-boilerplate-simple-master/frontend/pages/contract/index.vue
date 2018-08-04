001eae26e95366c78066ebec9d699b8369970637
Nathan Challen <nathan@payninja.co>
2018-08-04 23:35:33 +1000

    compiled and running on BC successfully

---
+++ eosio-project-boilerplate-simple-master/frontend/pages/contract/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-layout justify-center align-center fill-height>
+    <v-text-field v-model="lat" placeholder="latitude"/>
+    <v-text-field v-model="lng" placeholder="longitude"/>
+    <v-btn @click="postOffer">Post Offer</v-btn>
+  </v-layout>
+</template>
+
+<script>
+
+
+export default {
+  data: () => ({
+    lat: 0.0,
+    lng: 0.0,
+  }),
+  methods: {
+    postOffer(){
+        console.log(this.lat, this.lng);
+      //postOffer: {
+        //lat,lng,radius,desc,tokens    
+      //}
+    }  
+  }
+}
+</script>
